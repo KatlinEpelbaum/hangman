6c39ecad4450891d3e59a745b01572c68a0a49df
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2025-01-20 10:05:36 +0200

    update

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const scoreSpan = document.getElementById('score');
 const alphabetDiv = document.getElementById('alphabet');
 
 const alphabet = 'abcdefghijklmnopqrsšzžtuvwõäöüxy';
+let guessedLetters = '';
+
 for (letter of alphabet) {
     let letterSpan = document.createElement('span');
     letterSpan.id = letter;
     letterSpan.innerText = letter.toUpperCase();
 
+    letterSpan.addEventListener('click', e => {
+        if (!guessedLetters.includes(letter)) {
+            guessedLetters.push(letter);
+            console.log(letter);
+        }
+    })
+    
     alphabetDiv.appendChild(letterSpan);
 }
 
