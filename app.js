6a74e56b0d99b38860534b6fccbd5f0c90991930
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2025-01-20 09:22:45 +0200

    update

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-let word = 'i love cats';
+const guessedWordDiv = document.getElementById('word');
+const scoreSpan = document.getElementById('score');
+const alphabetDiv = document.getElementById('alphabet');
+
+const alphabet = 'abcdefghijklmnopqrsšzžtuvwõäöüxy';
+
+
+let score = 10;
+scoreDiv.innerText = score
+
+let word = 'I love cats <3';
+let guessedWord = '';
 
 for(let char of word){
-    console.log(char);
+    if( char.toUpperCase() != char.toLowerCase() ){
+        guessedWord += '_'
+    } else{
+        guessedWord+= char;
+    }
 }
+
+guessedWordDiv.innerText = guessedWord
+
+console.log(guessedWord);
